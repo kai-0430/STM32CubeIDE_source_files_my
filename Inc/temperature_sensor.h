2a6943181f7e5c724c3d93bad586e27570b45399
yft <frank23026407@gmail.com>
2023-04-22 15:20:09 +0800

    add temperator sensor

---
+++ Inc/temperature_sensor.h
@@ -0,0 +1,36 @@
+/*
+ * temperature_sensor.h
+ *
+ *  Created on: Apr 15, 2023
+ *      Author: frank
+ */
+
+#ifndef INC_TEMPERATURE_SENSOR_H_
+#define INC_TEMPERATURE_SENSOR_H_
+#include "stm32f4xx_hal.h"
+
+typedef unsigned char uint8_t;
+
+#define TEMP_ADDR1 (0x3F << 1) // I2C address should shift one
+
+class temperature_sensor
+{
+	/*
+	 * 	product name: 2525020210001
+	 *     	protocol: iic
+	 *     datasheet: https://www.we-online.com/components/products/manual/2525020210001_WSEN-HIDS%202525020210001%20Manual_rev1.6.pdf
+	 */
+private:
+	uint8_t addr;
+	I2C_HandleTypeDef* hi2c_p;
+	HAL_StatusTypeDef rv = HAL_OK;
+
+public:
+	temperature_sensor(I2C_HandleTypeDef* hi2c_pointer, uint8_t address);
+	bool init();
+	bool is_ready();
+	void set_count();
+	float get_temperature();
+};
+
+#endif /* INC_TEMPERATURE_SENSOR_H_ */
---
+++ Src/temperature_sensor.cpp
@@ -0,0 +1,56 @@
+/*
+ * temperature_sensor.cpp
+ *
+ *  Created on: Apr 15, 2023
+ *      Author: frank
+ *        Date: Apr 15, 2023
+ */
+
+#include "../../Source/Inc/temperature_sensor.h"
+
+//extern I2C_HandleTypeDef hi2c1;
+
+/*
+ * function name: temperature constructor
+ * input:
+ * 		* address: uint8_t, the i2c sensor of temperature sensor
+ * output:
+ * 		* none
+ * description:
+ * 		set the address of the temperature sensor
+ */
+temperature_sensor::temperature_sensor(I2C_HandleTypeDef* hi2c_pointer, uint8_t address)
+{
+	this->hi2c_p = hi2c_pointer;
+	this->addr = address;
+}
+
+bool temperature_sensor::init()
+{
+	uint8_t txbuff = 0x02;
+	rv = HAL_I2C_Mem_Write(hi2c_p, addr, 0x0C, I2C_MEMADD_SIZE_8BIT, &txbuff, 1, 1);
+	txbuff = 0x00;
+	rv = HAL_I2C_Mem_Write(hi2c_p, addr, 0x0C, I2C_MEMADD_SIZE_8BIT, &txbuff, 1, 1);
+	return rv == HAL_OK;
+}
+
+bool temperature_sensor::is_ready()
+{
+	rv = HAL_I2C_IsDeviceReady(hi2c_p, addr, 1, 10);
+	return rv == HAL_OK;
+}
+
+void temperature_sensor::set_count()
+{
+	uint8_t txbuff = 0x3C;
+	rv = HAL_I2C_Mem_Write(hi2c_p, addr, 0x04, I2C_MEMADD_SIZE_8BIT, &txbuff, 1, 10);
+}
+
+float temperature_sensor::get_temperature()
+{
+	uint8_t rxbuff[2];
+	int temp;
+	rv = HAL_I2C_Mem_Read(hi2c_p, addr, 0x06, I2C_MEMADD_SIZE_8BIT, rxbuff, 2, 10);
+	temp = rxbuff[1]<<8 | rxbuff[0];
+	return 0.01 * temp;
+}
